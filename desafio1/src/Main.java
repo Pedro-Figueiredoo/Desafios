f29cecf63eac583bf81660f112af7eed5a8849cb
Pedro-Figueiredo <pedroarthurft@gmail.com>
2024-04-03 20:26:16 -0300

    first commit

---
+++ desafio1/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número de entrada:"));
+
+        if (numero == 0) {
+            JOptionPane.showMessageDialog(null,"O seu número é Neutro");
+
+    } else if (numero < 0) {
+            JOptionPane.showMessageDialog(null,"O seu número é Negativo");
+
+    } else {
+            JOptionPane.showMessageDialog(null,"O seu número é Positivo");
+        }
+
+    }
+}
